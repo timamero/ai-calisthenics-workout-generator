88d06ab668905dab5c890442de4c04387be025bc
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-06-18 14:46:08 -0700

    Delete index.css file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
-import "./index.css";
+
 
 // Set up a Router instance
 const router = createRouter({

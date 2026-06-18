3e8ad0fd3af78ebdf01a38a0675aa36a88fea249
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-06-18 14:47:53 -0700

    Import mantine styles and wrap app in MantineProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
-import { routeTree } from "./routeTree.gen";
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
 
+import { routeTree } from "./routeTree.gen";
 
 // Set up a Router instance
 const router = createRouter({
@@ -24,7 +26,9 @@ if (!rootElement.innerHTML) {
   const root = createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <MantineProvider>
+        <RouterProvider router={router} />
+      </MantineProvider>
     </StrictMode>,
   );
 }

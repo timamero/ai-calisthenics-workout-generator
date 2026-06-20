a1ef3c490d75cba11be6f92071acbfa12e2ad91a
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-06-19 21:44:52 -0700

    Create store with sample state and action

---
+++ frontend/src/stores/useStore.ts
@@ -0,0 +1,11 @@
+import { create } from "zustand";
+
+interface SampleUseStoreState {
+  exercise: string;
+  addExercise: (name: string) => void;
+}
+
+export const useStore = create<SampleUseStoreState>((set) => ({
+  exercise: "",
+  addExercise: (name) => set({ exercise: name }),
+}));

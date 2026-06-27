7aa4890b8434f7c1d843613c8a95d9c698c6b6ea
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-06-26 19:25:48 -0700

    Configure CORS middleware

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",  # Local web server
+    "http://127.0.0.1:8000",  # Origin for the local FastAPI server
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")

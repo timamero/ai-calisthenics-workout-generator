de4747ab4d7331fa640973864670d65a53e73373
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-06-28 19:37:17 -0700

    Rename test function and move into class

--- backend/tests/api/routes/test_generate_workout.py
+++ backend/tests/api/routes/test_generate_workout.py
@@ -1,35 +1,39 @@
-async def test_generate_workout(client, sample_generated_workout):
-    """Test the successful creation of a generated workout."""
+class TestGenerateWorkout:
+    """Tests for generate_workout function."""
 
-    parameters = {
-        "equipment": ["pull up bar"],
-        "fitness_level": "beginner",
-        "target_muscles": ["upper back"],
-        "duration_minutes": 30,
-        "additional_notes": "additional notes",
-    }
+    async def test_generate_workout_valid_request(
+        self, client, sample_generated_workout
+    ):
+        """Test the successful creation of a generated workout."""
 
-    response = await client.request("POST", "/generate-workout/", json=parameters)
+        parameters = {
+            "equipment": ["pull up bar"],
+            "fitness_level": "beginner",
+            "target_muscles": ["upper back"],
+            "duration_minutes": 30,
+            "additional_notes": "additional notes",
+        }
 
-    assert response.status_code == 200
-    assert (
-        response.json()["workout"]["name"]
-        == sample_generated_workout["workout"]["name"]
-    )
-    assert response.json()["workout"]["equipment"] == parameters["equipment"]
+        response = await client.request("POST", "/generate-workout/", json=parameters)
 
+        assert response.status_code == 200
+        assert (
+            response.json()["workout"]["name"]
+            == sample_generated_workout["workout"]["name"]
+        )
+        assert response.json()["workout"]["equipment"] == parameters["equipment"]
 
-async def test_generate_workout_validation_error(client):
-    """Test that invalid request payloads return a 422 response."""
+    async def test_generate_workout_validation_error(self, client):
+        """Test that invalid request payloads return a 422 response."""
 
-    parameters = {
-        "equipment": "pull up bar",
-        "fitness_level": "beginner",
-        "target_muscles": "upper back",
-        "duration_minutes": "PT30M",
-        "additional_notes": "additional notes",
-    }
+        parameters = {
+            "equipment": "pull up bar",
+            "fitness_level": "beginner",
+            "target_muscles": "upper back",
+            "duration_minutes": "PT30M",
+            "additional_notes": "additional notes",
+        }
 
-    response = await client.request("POST", "/generate-workout/", json=parameters)
+        response = await client.request("POST", "/generate-workout/", json=parameters)
 
-    assert response.status_code == 422
+        assert response.status_code == 422

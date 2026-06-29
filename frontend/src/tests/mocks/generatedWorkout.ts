ad41c7197fc4a1c53012940533ae0667d9a8a585
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-06-28 23:49:50 -0700

    Create mock response for testing

---
+++ frontend/src/tests/mocks/generatedWorkout.ts
@@ -0,0 +1,141 @@
+export const mockGenerateWorkoutResponse = {
+  workout: {
+    name: 'Generated workout name',
+    equipment: ['pull-up-bar', 'rings'],
+    fitness_level: 'intermediate',
+    target_muscles: ['back', 'core'],
+    duration_minutes: 30,
+    additional_notes: 'Avoid overhead pressing, shoulder issue',
+    workout_data: {
+      data: [
+        {
+          id: '00000000-0000-0000-0000-000000000000',
+          sets: [
+            {
+              id: '00000000-0000-0000-0000-000000000000',
+              fields: {
+                reps: 0,
+                rest: 'string',
+                time: 'string',
+                setProgressions: [
+                  {
+                    id: '00000000-0000-0000-0000-000000000000',
+                    value: 0,
+                    set_progression_id: 0,
+                  },
+                ],
+              },
+              completed: true,
+              completed_at: 'string',
+            },
+          ],
+          type: 'exercise',
+          order: 0,
+          tracked: ['string'],
+          exercise_id: 0,
+        },
+        {
+          id: '00000000-0000-0000-0000-000000000000',
+          name: 'string',
+          type: 'section',
+          items: [
+            {
+              id: '00000000-0000-0000-0000-000000000000',
+              sets: [
+                {
+                  id: '00000000-0000-0000-0000-000000000000',
+                  fields: {
+                    reps: 0,
+                    rest: 'string',
+                    time: 'string',
+                    setProgressions: [
+                      {
+                        id: '00000000-0000-0000-0000-000000000000',
+                        value: 0,
+                        set_progression_id: 0,
+                      },
+                    ],
+                  },
+                  completed: true,
+                  completed_at: 'string',
+                },
+              ],
+              type: 'exercise',
+              order: 0,
+              tracked: ['string'],
+              exercise_id: 0,
+            },
+            {
+              id: '00000000-0000-0000-0000-000000000000',
+              type: 'superset',
+              order: 0,
+              exercises: [
+                {
+                  id: '00000000-0000-0000-0000-000000000000',
+                  sets: [
+                    {
+                      id: '00000000-0000-0000-0000-000000000000',
+                      fields: {
+                        reps: 0,
+                        rest: 'string',
+                        time: 'string',
+                        setProgressions: [
+                          {
+                            id: '00000000-0000-0000-0000-000000000000',
+                            value: 0,
+                            set_progression_id: 0,
+                          },
+                        ],
+                      },
+                      completed: true,
+                      completed_at: 'string',
+                    },
+                  ],
+                  type: 'exercise',
+                  order: 0,
+                  tracked: ['string'],
+                  exercise_id: 0,
+                },
+              ],
+            },
+          ],
+          order: 0,
+        },
+        {
+          id: '00000000-0000-0000-0000-000000000000',
+          type: 'superset',
+          order: 0,
+          exercises: [
+            {
+              id: '00000000-0000-0000-0000-000000000000',
+              sets: [
+                {
+                  id: '00000000-0000-0000-0000-000000000000',
+                  fields: {
+                    reps: 0,
+                    rest: 'string',
+                    time: 'string',
+                    setProgressions: [
+                      {
+                        id: '00000000-0000-0000-0000-000000000000',
+                        value: 0,
+                        set_progression_id: 0,
+                      },
+                    ],
+                  },
+                  completed: true,
+                  completed_at: 'string',
+                },
+              ],
+              type: 'exercise',
+              order: 0,
+              tracked: ['string'],
+              exercise_id: 0,
+            },
+          ],
+        },
+      ],
+    },
+  },
+  remaining_generations: 20,
+};
